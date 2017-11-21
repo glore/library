5da7986b76e953d8ed23fd8b8798a3b6795526e4
Glore Zeng <zeng.glore@gmail.com>
2017-11-22 00:16:08 +0800

    增加图书推荐

--- src/config.js
+++ src/config.js
@@ -3,16 +3,14 @@
 ======================================================== */
 
 // 域名
-var host = 'https://spark.ufutx.com/api';
+var host = 'https://library.ufutx.com/api';
 // var host = 'http://library.glore/api';
 
 export const service = {
     // 登录接口
     login: `${host}/login/wechat`,
-
     // 微信注册接口
     register: `${host}/wechat/register`,
-
     // 注册发短信接口
     send_register: `${host}/sms/register`,
 
@@ -24,25 +22,22 @@ export const service = {
 
     // 列表接口 GET
     list: `${host}/books`,
-
     // 图书馆接口
     libraries: `${host}/libraries`,
     // 图书馆接口
     library: `${host}/libraries/1`,
 
+    recommend: `${host}/recommend`,
+
 
     // 图书馆图书录入
     book_store: `${host}/books`,
-
     // 图书列表
     books: `${host}/books`,
-
     // 图书分类
     sorts: `${host}/sorts`,
-
     // 筛选页接口 GET
     tags: `${host}/bookmall/tags`,
-
     // 假装有收藏接口 POST
     collect: `${host}/bookmall/list`,
 
