cfee62ce3149f901939725cb881dc2d7b3b0ee44
DZF <1224437817@qq.com>
2018-03-14 09:53:39 +0800

    更换icon

--- src/config.js
+++ src/config.js
@@ -3,44 +3,42 @@
 ======================================================== */
 
 // 域名
-// var host = 'https://library.ufutx.com/api'
-var host = 'http://library.glore/api'
 
 export const service = {
-  // 登录接口
-  login: `${host}/login/wechat`,
-  // 微信注册接口
-  register: `${host}/wechat/register`,
-  // 注册发短信接口
-  send_register: `${host}/sms/register`,
+    // 登录接口
+    login: `${host}/login/wechat`,
+    // 微信注册接口
+    register: `${host}/wechat/register`,
+    // 注册发短信接口
+    send_register: `${host}/sms/register`,
 
-  // 用户
-  user: `${host}/user`,
+    //用户
+    user: `${host}/user`,
 
-  // 借阅书籍
-  reserve: `${host}/libraries/books`,
+    //借阅书籍
+    reserve: `${host}/libraries/books`,
 
-  // 列表接口 GET
-  list: `${host}/books`,
-  // 图书馆接口
-  libraries: `${host}/libraries`,
-  // 图书馆接口
+    // 列表接口 GET
+    list: `${host}/books`,
+    // 图书馆接口
+    libraries: `${host}/libraries`,
+    // 图书馆接口
   my_libraries: `${host}/user/libraries`,
-  recommend: `${host}/recommend`,
-  // 图书馆图书录入
-  book_store: `${host}/books`,
-  // 图书列表
-  books: `${host}/books`,
-  // 图书分类
-  sorts: `${host}/sorts`,
-  // 筛选页接口 GET
-  tags: `${host}/bookmall/tags`,
-  // 假装有收藏接口 POST
-  collect: `${host}/bookmall/list`,
-  // 主域
-  host
+    recommend: `${host}/recommend`,
+    // 图书馆图书录入
+    book_store: `${host}/books`,
+    // 图书列表
+    books: `${host}/books`,
+    // 图书分类
+    sorts: `${host}/sorts`,
+    // 筛选页接口 GET
+    tags: `${host}/bookmall/tags`,
+    // 假装有收藏接口 POST
+    collect: `${host}/bookmall/list`,
+    // 主域
+    host
 }
 
 export default {
-  service
+    service
 }
