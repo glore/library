339e58296a9ac3e7285f38a0ee57447af2610dfd
DZF <dengzhifeng_63@163.com>
2018-09-12 16:04:31 +0800

    测试

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@
                         小程序配置文件
 ======================================================== */
 // 域名
-var host = 'https://library.ufutx.com/api'
-// var host = 'http://library.hankin.ufutx.cn/api'
+// var host = 'https://library.ufutx.com/api'
+var host = 'http://library.hankin.ufutx.cn/api'
 export const service = {
     // 登录接口
     login: `${host}/login/wechat`,
